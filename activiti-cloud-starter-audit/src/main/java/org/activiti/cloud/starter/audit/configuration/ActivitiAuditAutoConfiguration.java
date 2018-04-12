82884824e191fc385ddc764b937d37b19b0c724f
Ryan Dawson <ryan.dawson@alfresco.com>
2018-04-12 11:30:02 +0100

    componentscan needs to include identity

--- activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
+++ activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan({"org.activiti.cloud.services.audit","org.activiti.cloud.services.security"})
+@ComponentScan({"org.activiti.cloud.services.audit","org.activiti.cloud.services.security","org.activiti.cloud.services.identity","org.activiti.cloud.alfresco"})
 public class ActivitiAuditAutoConfiguration {
 
 }
