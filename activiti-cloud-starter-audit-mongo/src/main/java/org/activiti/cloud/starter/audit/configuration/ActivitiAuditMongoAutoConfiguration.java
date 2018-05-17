4bfe609414eba12e60a97336b8b0b78cf78b498a
Ryan Dawson <ryan.dawson@alfresco.com>
2018-05-17 16:49:38 +0100

    need to make securitypolicesservice available

--- activiti-cloud-starter-audit-mongo/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditMongoAutoConfiguration.java
+++ activiti-cloud-starter-audit-mongo/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditMongoAutoConfiguration.java
@@ -1,10 +1,18 @@
 package org.activiti.cloud.starter.audit.configuration;
 
+import org.activiti.cloud.services.security.SecurityPoliciesApplicationService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan({"org.activiti.cloud.services.audit"})
 public class ActivitiAuditMongoAutoConfiguration {
-
+    
+    @Bean
+    @ConditionalOnMissingBean
+    public SecurityPoliciesApplicationService securityPoliciesApplicationService(){
+        return new SecurityPoliciesApplicationService();
+    }
 }
--- activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
+++ activiti-cloud-starter-audit/src/main/java/org/activiti/cloud/starter/audit/configuration/ActivitiAuditAutoConfiguration.java
@@ -1,5 +1,8 @@
 package org.activiti.cloud.starter.audit.configuration;
 
+import org.activiti.cloud.services.security.SecurityPoliciesApplicationService;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySources;
@@ -8,5 +11,11 @@ import org.springframework.context.annotation.PropertySource;
 @Configuration
 @ComponentScan({"org.activiti.cloud.services.audit"})
 public class ActivitiAuditAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public SecurityPoliciesApplicationService securityPoliciesApplicationService(){
+        return new SecurityPoliciesApplicationService();
+    }
 
 }
