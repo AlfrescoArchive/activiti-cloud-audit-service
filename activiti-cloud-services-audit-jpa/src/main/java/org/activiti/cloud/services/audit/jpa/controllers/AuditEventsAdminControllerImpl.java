14ea4b950308f5ff824584128d61dd2f15f7dec7
Alex Merkulov <almerico@gmail.com>
2019-07-26 18:56:55 +0300

    update to use search

--- activiti-cloud-services-audit-jpa/src/main/java/org/activiti/cloud/services/audit/jpa/controllers/AuditEventsAdminControllerImpl.java
+++ activiti-cloud-services-audit-jpa/src/main/java/org/activiti/cloud/services/audit/jpa/controllers/AuditEventsAdminControllerImpl.java
@@ -19,22 +19,30 @@ package org.activiti.cloud.services.audit.jpa.controllers;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.google.common.base.Joiner;
 import org.activiti.api.runtime.shared.NotFoundException;
 import org.activiti.cloud.alfresco.data.domain.AlfrescoPagedResourcesAssembler;
 import org.activiti.cloud.api.model.shared.events.CloudRuntimeEvent;
 import org.activiti.cloud.services.audit.api.assembler.EventResourceAssembler;
 import org.activiti.cloud.services.audit.api.controllers.AuditEventsAdminController;
 import org.activiti.cloud.services.audit.api.converters.APIEventToEntityConverters;
+import org.activiti.cloud.services.audit.api.converters.EventToEntityConverter;
 import org.activiti.cloud.services.audit.api.resources.EventsRelProvider;
 import org.activiti.cloud.services.audit.jpa.events.AuditEventEntity;
+import org.activiti.cloud.services.audit.jpa.repository.EventSpecificationsBuilder;
 import org.activiti.cloud.services.audit.jpa.repository.EventsRepository;
+import org.activiti.cloud.services.audit.jpa.repository.SearchOperation;
 import org.activiti.cloud.services.audit.jpa.security.SecurityPoliciesApplicationServiceImpl;
 import org.activiti.core.common.spring.security.policies.ActivitiForbiddenException;
+import org.activiti.core.common.spring.security.policies.SecurityPolicyAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.hateoas.Resource;
@@ -42,6 +50,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -64,7 +73,7 @@ public class AuditEventsAdminControllerImpl implements AuditEventsAdminControlle
                                           APIEventToEntityConverters eventConverters,
                                           AlfrescoPagedResourcesAssembler<CloudRuntimeEvent> pagedResourcesAssembler,
                                           SecurityPoliciesApplicationServiceImpl securityPoliciesApplicationService
-                                          ) {
+    ) {
         this.eventsRepository = eventsRepository;
         this.eventResourceAssembler = eventResourceAssembler;
         this.eventConverters = eventConverters;
@@ -73,13 +82,20 @@ public class AuditEventsAdminControllerImpl implements AuditEventsAdminControlle
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public PagedResources<Resource<CloudRuntimeEvent>> findAll(Pageable pageable) {
-        Page<AuditEventEntity> allAuditInPage = eventsRepository.findAll(pageable);
+    public PagedResources<Resource<CloudRuntimeEvent>> findAll(@RequestParam(value = "search", required = false) String search,
+                                                               Pageable pageable) {
 
+        Specification<AuditEventEntity> spec = createSearchSpec(search);
+
+        Page<AuditEventEntity> allAuditInPage = eventsRepository.findAll(spec,
+                                                                         pageable);
         List<CloudRuntimeEvent> events = new ArrayList<>();
 
         for (AuditEventEntity aee : allAuditInPage.getContent()) {
-            events.add(eventConverters.getConverterByEventTypeName(aee.getEventType()).convertToAPI(aee));
+            EventToEntityConverter converterByEventTypeName = eventConverters.getConverterByEventTypeName(aee.getEventType());
+            if (converterByEventTypeName != null) {
+                events.add(converterByEventTypeName.convertToAPI(aee));
+            }
         }
 
         return pagedResourcesAssembler.toResource(pageable,
@@ -98,5 +114,24 @@ public class AuditEventsAdminControllerImpl implements AuditEventsAdminControlle
         AuditEventEntity auditEventEntity = findResult.get();
         CloudRuntimeEvent cloudRuntimeEvent = eventConverters.getConverterByEventTypeName(auditEventEntity.getEventType()).convertToAPI(auditEventEntity);
         return eventResourceAssembler.toResource(cloudRuntimeEvent);
+    }
+
+    private Specification<AuditEventEntity> createSearchSpec(String search) {
+        EventSpecificationsBuilder builder = new EventSpecificationsBuilder();
+        if (search != null && !search.isEmpty()) {
+            String operationSetExper = Joiner.on("|")
+                    .join(SearchOperation.SIMPLE_OPERATION_SET);
+            Pattern pattern = Pattern.compile("(\\w+?)(" + operationSetExper + ")(\\p{Punct}?)([a-zA-Z0-9-_]+?)(\\p{Punct}?),");
+            Matcher matcher = pattern.matcher(search + ",");
+            while (matcher.find()) {
+                builder.with(matcher.group(1),
+                             matcher.group(2),
+                             matcher.group(4),
+                             matcher.group(3),
+                             matcher.group(5));
+            }
+        }
+
+        return builder.build();
     }
 }
